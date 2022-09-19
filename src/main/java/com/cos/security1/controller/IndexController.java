a3a616c34dbc1b3099d7b638ad290efe0e29ac16
MoonMinHyuk1 <moon981018@gmail.com>
2022-09-20 01:48:25 +0900

    section0-5

---
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -0,0 +1,65 @@
+package com.cos.security1.controller;
+
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class IndexController {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping({"", "/"})
+    public String index() {
+        //머스테치 기본폴더 src/main/resources/
+        //뷰리졸버 설정 : templates (prefix), .mustache (suffix) application 파일에서 생략가
+        return "index"; // src/main/resources/templates/index.mustache
+    }
+
+    @GetMapping("/user")
+    @ResponseBody
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    @ResponseBody
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    @ResponseBody
+    public String manager() {
+        return "manager";
+    }
+
+    //spring security 가 주소를 낚아챔 - SecurityConfig 파일 생성 후 작동안함
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "loginForm";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "joinForm";
+    }
+
+    @PostMapping("/join")
+    public String join(User user) {
+        System.out.println("user = " + user);
+        user.setRole("ROLE_USER");
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        userRepository.save(user); //회원가입 잘됨. 비밀번호 : 1234 -> 시큐리티로 로그인을 할 수 없음. 이유는 패스워드가 암호화가 안돼있기 때문
+
+        return "redirect:/loginForm";
+    }
+}
